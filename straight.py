da59423f3c04f061b8a0b54fa6ddf241e0418b6e
Carolina Figueroa <andreacarolina.f28@gmail.com>
2024-04-03 22:46:16 -0600

    .

--- straight.py
+++ straight.py
@@ -12,12 +12,12 @@ fw.offset = 10
 
 bw.speed = 0
 bw.backward()
-bw.speed = 50
+bw.speed = 80
 
 count = 0
 
 while count < 100:
-    count += 0.001
+    count += 0.005
     print(count)
     if count > 50 and count < 60:
         fw.turn(40)
