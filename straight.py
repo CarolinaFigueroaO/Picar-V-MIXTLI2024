97105c6b7d9950ab0e5943cbee54d3594723a541
Carolina Figueroa <andreacarolina.f28@gmail.com>
2024-04-03 22:39:23 -0600

    .

--- straight.py
+++ straight.py
@@ -6,13 +6,13 @@ import picar
 picar.setup()
 bw = back_wheels.Back_Wheels()
 fw = front_wheels.Front_Wheels()
+
 fw.offset = 10
 fw.turn(90)
 
-bw.speed = 0
-bw.backward()
 
 bw.speed = 50
-while True:
-    pass
+bw.backward()
+
+fw.turn(45)
 
