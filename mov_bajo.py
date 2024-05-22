031aea1ecbccdfce871dc0b88a1d497032fa864f
Carolina Figueroa <andreacarolina.f28@gmail.com>
2024-05-21 23:36:08 -0600

    .

--- mov_bajo.py
+++ mov_bajo.py
@@ -175,7 +175,7 @@ def evitLines(mask):
 def main():
     # Suponiendo que estás capturando video desde una cámara
     cap = cv2.VideoCapture(0)
-    bw.speed = 30
+    bw.speed = 40
     createTrackbars()
     while True:
         ret, frame = cap.read()
