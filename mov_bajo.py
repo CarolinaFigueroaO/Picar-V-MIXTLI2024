3f2232126e463ba4bb8018955555def3efa08940
Carolina Figueroa <andreacarolina.f28@gmail.com>
2024-05-21 23:21:08 -0600

    .

--- mov_bajo.py
+++ mov_bajo.py
@@ -120,10 +120,10 @@ def evitLines(mask):
                 width = mask.shape[1]
 
                 # Decide la dirección del movimiento basado en la posición X
-                if cX  < width // 3 and cX > width // 4:
+                if cX < width // 2 and cX > width // 4:
                     direction = "Girar a la derecha"
                     fw.turn(180)
-                elif cX > 2 * width // 3 and cX < 3 * width // 4:
+                elif cX > width // 2 and cX < 3 * width // 4:
                     direction = "Girar a la izquierda"
                     fw.turn(0)
 
